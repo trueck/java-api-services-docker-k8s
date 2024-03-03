edf8a6c60e9d68a5dee2d8070c7296f656f29a60
卢炳耀 <edwinlu@lubingyaodeMacBook-Air.local>
2024-03-03 20:18:53 +0800

    implement recommendation service

--- recommendation-service/src/main/java/com/example/recommendation/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/com/example/recommendation/RecommendationServiceApplication.java
@@ -1,13 +1,14 @@
-package com.example.recommendationservice;
+package com.example.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecommendationServiceApplication.class, args);
 	}
-
 }
