fee7cbd36c11c8a0db6345642c09f5915f5f3689
卢炳耀 <edwinlu@lubingyaodeMacBook-Air.local>
2024-03-05 17:49:03 +0800

    add api descriptions in swagger

--- my-api/src/main/java/com/example/api/composite/product/ProductCompositeService.java
+++ my-api/src/main/java/com/example/api/composite/product/ProductCompositeService.java
@@ -1,8 +1,13 @@
 package com.example.api.composite.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Tag(name="ProductComposite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
 
     /**
@@ -11,6 +16,14 @@ public interface ProductCompositeService {
      * @param productId
      * @return the composite product info, if found, else null
      */
+    @Operation(
+            summary = "${api.product-composite.get-composite-product.description}",
+        description = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Bad Request, invalid format of the request. See response message for more information."),
+                @ApiResponse(responseCode = "404", description = "Not found, the specified id does not exist."),
+                @ApiResponse(responseCode = "422", description = "Unprocessable entity, input parameters caused the processing to fails. See response message for more information.")
+                })
     @GetMapping(
         value    = "/product-composite/{productId}",
         produces = "application/json")
